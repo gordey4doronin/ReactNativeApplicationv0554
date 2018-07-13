3585c32c3a43fdb464905587ee0787ffc4954461
Evgenii Khramkov <evkhramk@microsoft.com>
2018-07-13 21:31:22 +0300

    test event and test crash

--- App.js
+++ App.js
@@ -1,40 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
+import * as Crashes from 'appcenter-crashes';
+import * as Analytics from 'appcenter-analytics';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button title='test event' onPress={this.sendTestEvent}/>
+        <Button title='test crash' onPress={this.sendTestCrash}/>
       </View>
     );
+  }
+
+  sendTestCrash = () => {
+    Crashes.generateTestCrash();
+  }
+
+  sendTestEvent = () => {
+    Analytics.trackEvent("test", {});
   }
 }
 
@@ -44,15 +34,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
